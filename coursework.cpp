b0750162038a397f2e2f1fbe0061561ebbae5685
Thomas Auttenberg <senior.auttenberg@gmail.com>
2023-09-22 08:47:04 +0700

    conflicts

--- coursework.cpp
+++ coursework.cpp
@@ -11,54 +11,50 @@
 #include <sstream>
 #include <list>
 #include "clist.h"
+#include "Menu.h"
 
 int main()
 {
-
-	//menu: 
-
 	float ca = 0;
 	ca++;
 	std::ifstream is;
 	void* n;
 	int sss = 0;
-	clist<float> s = { 12,3,4,5 };
-	clist<float>::iterator a = s.begin();
-	auto dd = a;
-	dd++;
-	dd++;
-	s.erase(a, dd);
-	
-	const std::list<int> sssss;
-
-	s.foreach([&](clist<float>::elem value){
+	clist<float> list = { 13,12,5,3,5 };
+	list.foreach([](float s) {
 		
-		value++;
-		sss += 1;
+		
 
+		s.foreach([&](clist<float>::elem value) {
+			})
+
+	Menu a("How are you today?");
+	Menu b([&]()-> void {
+		
+		list.foreach([](float s) {
+			std::cout << s << " ";
+			});
 	});
-	s.foreach([&](clist<float>::elem value) {
-
-		std::cout << value;
-
+	MenuItem a1("How's it",b);
+	a.addItem(a1);
+	MenuItem b1("Delete the last", [&]() {
+		list.pop_back();
 	});
-	clist<float>::iterator z = s.begin();
-	clist<float>::iterator v = s.begin();
-	s.erase(s.begin() , s.begin());
-	std::function<void(const clist<int>::iterator)> h;
+	b.addItem(b1);
 	
-	/*//clist<int>::iterator k;
-	is.open("meow");
+	/* 
+	//clist<int>::iterator k;
+	//is.open("meow");
 	//Float c(12.5);
 	//Float* s = new Float[25];
 	//char* str = c.to_cstring();
-	str[3] = '2';
-	Float k;
-	Int csd;
-	k.from_cstring(str);
-	std::string sss;;
-	std::cout << typeid(c).hash_code() << " " << typeid(csd).hash_code();
-	*/
+	//str[3] = '2';
+	//Float k;
+	//Int csd;
+	//k.from_cstring(str);
+	//std::string sss;;
+	//std::cout << typeid(c).hash_code() << " " << typeid(csd).hash_code();
+	//*/
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
